435b9a49a9ec37a022ac452b6e267c7bd6b2bca0
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-06-29 11:41:16 +0100

    Modified Protocols

--- protocols/manifests/init.pp
+++ protocols/manifests/init.pp
@@ -5,25 +5,20 @@ class protocols {
   $token = "TOKEN_HERE"
 
   file { 'copy_source':
-    path => '/root/Protocol1Server.java',
+    path => '/tmp/Protocol1Server.java',
     content => template('protocols/Protocol1Server.java.erb'),
     owner => 'root',
     group => 'root',
     mode => '0700',
+    before => Exec['compile_server']
   }
 
   exec { "compile_server":
-    command => 'javac Protocol1Server.java',
+    command => 'javac Protocol1Server.java &&
+                mv Protocol1Server.class /root/Protocol1Server.class &&
+                rm Protocol1Server.java',
     path => '/usr/bin/:/bin',
-    cwd => '/root',
-    require => File["copy_source"],
-  }
-
-  exec { 'delete_source':
-   command => 'rm Protocol1Server.java',
-   path => '/usr/bin/:/bin',
-   cwd => '/root',
-   require => Exec["compile_server"],
+    cwd => '/tmp',
   }
 
 }
