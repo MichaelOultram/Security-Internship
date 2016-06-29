70abbd56b88b51a80d3b8a8c57a0d036e5955bfe
Paul Popa <paul.popa1995@yahoo.ro>
2016-06-29 15:04:01 +0100

    Moved the declaration for the protocols

--- protocols/manifests/init.pp
+++ protocols/manifests/init.pp
@@ -1,29 +1,29 @@
-class protocols { 
+class protocols ($protocols) { 
 
 # one-off defined resource type, in
 # /etc/puppetlabs/code/environments/production/modules/puppet/manifests/binary/symlink.pp
-define puppet::binary::symlink ($binary = $title) {
-  file {"${binary}":
-    path => "/tmp/${binary}.java",
-    content => template("protocols/${binary}.java.erb"),
+define puppet::binary::symlink ($protocol = $title) {
+  file {"${protocol}":
+    path => "/tmp/${protocol}.java",
+    content => template("protocols/${protocol}.java.erb"),
     #target => "/opt/puppetlabs/bin/${binary}",
     owner => 'root',
     group => 'root',
     mode => '0700',
-    before => Exec["compile_server${binary}"]
+    before => Exec["compile_server${protocol}"]
   }
-  exec { "compile_server${binary}":
-	command => "javac ${binary}.java &&
-	mv ${binary}.class /root/${binary}.class &&
-	rm ${binary}.java",
+  exec { "compile_server${protocol}":
+	command => "javac ${protocol}.java &&
+	mv ${protocol}.class /root/${protocol}.class &&
+	rm ${protocol}.java",
         path => '/usr/bin/:/bin',
 	cwd => '/tmp',
   }
 }
 
 # using defined type for iteration, somewhere else in your manifests
-$binaries = ["Protocol1Server", "Protocol2Server"]
+#$protocols = ["Protocol1Server", "Protocol2Server"]
 
-puppet::binary::symlink { $binaries: }
+puppet::binary::symlink { $protocols: }
 }
 
