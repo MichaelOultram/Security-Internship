74549eb732a7d86f3363f2f5c7eede85cf25b144
Michael Oultram <MichaelOultram@users.noreply.github.com>
2016-07-27 13:57:03 +0100

    Metasploit puppet module works

--- metasploit/manifests/init.pp
+++ metasploit/manifests/init.pp
@@ -1,35 +1,57 @@
 class metasploit(){
+  include rvm
 
-  exec { "update":
-		command => "sudo apt-get update",
+
+  /*exec { "update":
+		command => "apt-get update",
 		path => '/usr/bin/:/bin',
 	}->
 
   exec { "upgrade":
-		command => "sudo apt-get upgrade",
+		command => "apt-get upgrade -y",
 		path => '/usr/bin/:/bin',
-	}->
+	}->*/
 
-  exec {"packages"
-    command => "sudo apt-get install nmap build-essential libreadline-dev libssl-dev libpq5 libpq-dev libreadline5 libsqlite3-dev libpcap-dev openjdk-7-jre git-core autoconf postgresql pgadmin3 curl zlib1g-dev libxml2-dev libxslt1-dev vncviewer libyaml-dev curl zlib1g-dev",
-    path =>'/usr/bin/:/bin'
+  rvm_system_ruby { 'ruby-2.3.1':
+    ensure      => 'present',
+    default_use => true,
+  }
+
+  rvm_gem { 'bundler':
+    name         => 'bundle',
+    ruby_version => 'ruby-2.3.1',
+    ensure       => latest,
+    require      => Rvm_system_ruby['ruby-2.3.1'];
+  }
+
+  #$packages = ["nmap", "build-essential", "libreadline-dev", "libssl-dev", "libpq5", "libpq-dev", "libreadline5", "libsqlite3-dev", "libpcap-dev", "openjdk-7-jre", "git-core", "autoconf", "postgresql", "pgadmin3", "curl", "zlib1g-dev", "libxml2-dev", "libxslt1-dev", "vncviewer", "libyaml-dev"]
+  $packages = ["nmap", "build-essential", "libreadline-dev", "libssl-dev", "libpq5", "libpq-dev", "libreadline5", "libsqlite3-dev", "libpcap-dev", "openjdk-7-jre", "git-core", "autoconf", "postgresql", "pgadmin3", "zlib1g-dev", "libxml2-dev", "libxslt1-dev", "vncviewer", "libyaml-dev"]
+  package { $packages:
+    ensure => installed,
   }->
 
-  exec {"clone_metasploit"
-    command => "sudo git clone https://github.com/rapid7/metasploit-framework.git"
-    path =>'usr/bin/:/bin'
-    cwd =>'/opt'
+  exec {"clone_metasploit":
+    command => "git clone https://github.com/rapid7/metasploit-framework.git",
+    provider => "shell",
+    creates => "/opt/metasploit-framework",
+    cwd =>'/opt',
   }->
 
-  exec {"chage_owner"
-    command => "sudo chown -R `whoami` /opt/metasploit-framework"
-    path =>'usr/bin/:/bin'
+  exec {"chage_owner":
+    command => "chown -R root /opt/metasploit-framework",
+    path =>'usr/bin/:/bin',
   }->
 
-  exec {"bundle"
-    command => "bundle install"
-    path =>'usr/bin/:/bin'
-    cwd =>'/opt/metasploit-framework'
+  /*package { 'bundle':
+    provider => "gem",
+    ensure => installed,
+  }->*/
+
+  exec {"bundle":
+    command => '/bin/bash --login -c "gem install bundle && bundle install"',
+    path =>'/usr/local/rvm/bin:/usr/local/rvm/rubies/ruby-2.3.1/bin:/usr/bin/:/bin:/usr/local/bin',
+    cwd =>'/opt/metasploit-framework',
+    require => Rvm_gem['bundler'], #Rvm_system_ruby['ruby-2.3.1'],
   }
 
 }
--- phishing/manifests/init.pp
+++ phishing/manifests/init.pp
@@ -1,13 +1,6 @@
 class phishing($ip_start = "172.18.0") {
   include puppetdocker
-
-  package { 'postfix':
-    ensure => installed,
-  }
-  service { 'postfix':
-    ensure => running,
-    require => Package['postfix'],
-  }
+  include phishing::mailserver
 
   puppetdocker::network {"phishing":
     cidr => "${ip_start}.0/24",
@@ -25,18 +18,61 @@ class phishing($ip_start = "172.18.0") {
 
 }
 
+class phishing::mailserver {
+  package { 'postfix':
+    ensure => installed,
+  }
+  package { 'dovecot-core':
+    ensure => installed,
+    notify => Service['postfix'],
+  }
+  package { 'dovecot-imapd':
+    ensure => installed,
+    notify => Service['postfix'],
+  }
+  file_line { "Plaintext authentication": # Because who needs encryption....
+    line => "disable_plaintext_auth = no",
+    path => "/etc/dovecot/dovecot.conf",
+    require => [Package['dovecot-core'], Package['dovecot-imapd']],
+    notify => Service['dovecot'],
+  }
+
+  file_line { "Local Emails Only":
+    line => "disable_dns_lookups=yes",
+    path => "/etc/postfix/main.cf",
+    require => Package['postfix'],
+    notify => Service['postfix'],
+  }
+
+  file_line { "No tls on SMTP":
+    line => "smtpd_use_tls=no",
+    match => "^smtpd_use_tls=yes$",
+    path => "/etc/postfix/main.cf",
+    require => Package['postfix'],
+    notify => Service['postfix'],
+  }
+
+  service { 'postfix':
+    ensure => running,
+    require => Package['postfix'],
+  }
+
+  service { 'dovecot':
+    ensure => running,
+    require => File_line["Plaintext authentication"],
+  }
+}
+
 # Defines all the node definitions for all of the containers defined above
 class phishing::nodes ($ip_start = "172.18.0") {
 
   node "phishing.vm" {
     class { "gateway": }
+    include phishing::mailserver
 
-    package { 'postfix':
-      ensure => installed,
-    }
-    service { 'postfix':
-      ensure => running,
-      require => Package['postfix'],
+    file { "/root/startup/mailserver.sh":
+      mode => "700",
+      content => "#!/bin/bash\nservice postfix start\nservice dovecot\n",
     }
 
     user { 'victim':
@@ -46,8 +82,27 @@ class phishing::nodes ($ip_start = "172.18.0") {
     }
   }
 
-  node "comp1.acl.vm" {
+  node "comp1.phishing.vm" {
     include java
+
+    file { "/root/mail.jar":
+      source => "puppet:///modules/phishing/mail.jar",
+    }
+
+    $email_server = "phishing.vm"
+    $email_user = "victim"
+    $email_pass = "victim"
+
+    file { "/root/LectureEx.java":
+      content => template("phishing/LectureEx.java.erb"),
+    }
+
+    exec { 'compile LectureEx':
+      command => 'javac -cp mail.jar LectureEx.java',
+      provider => "shell",
+      cwd => "/root",
+      require => [File['/root/mail.jar'], File["/root/LectureEx.java"]],
+    }
 
   }
 
