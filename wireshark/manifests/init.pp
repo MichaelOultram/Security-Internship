42ffb4968e5965a08f1fe2beeea03bf3e047d85b
Sam Holdcroft <sam@debian>
2016-07-29 14:45:13 +0100

    Stopped wireshark from making a link to the charlie directory

--- wireshark/manifests/init.pp
+++ wireshark/manifests/init.pp
@@ -28,12 +28,6 @@ class wireshark {
 	}
 
 	package { 'wireshark':
-		ensure => present,
-	  before => File["/home/charlie/wireshark"],
-	}
-
-	file { '/home/charlie/wireshark':
-		ensure => 'link',
-		target => '/usr/bin/wireshark',
+		ensure => present
   }
 }
